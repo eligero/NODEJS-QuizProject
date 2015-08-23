bc103312a013ef1d2fd62b4439f054aae4764ca4
EstebanLiG <esteban.elig@gmail.com>
2015-08-24 00:24:46 +0200

    Remote Postgres DB adaptation

--- models/models.js
+++ models/models.js
@@ -1,12 +1,29 @@
 var path = require('path');
+
 /* Load ORM model*/
 var Sequelize = require('sequelize');
 
-/* DB SQLite */
+//Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+//SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+/* DB SQLite or Postgres*/
 var sequelize = new Sequelize(
-  null, null, null,
-  {dialect: "sqlite",
-   storage: "quizDB.sqlite"}
+  DB_name, user, pwd, {dialect: protocol,
+                       protocol: protocol,
+                       port: port,
+                       host: host,
+                       storage: storage,//SQLite (.env file)
+                       omitNull: true //Postgres
+                       }
 );
 
 /* Import Quiz table definition on quiz.js */
